20b2ebe91bed1bdb868420905c511df54e1b2378
TonyBimenyi <tonyblaisb@gmail.com>
2022-06-24 06:30:44 -0700

    all

--- resources/views/etudiant/list_etudiant.blade.php
+++ resources/views/etudiant/list_etudiant.blade.php
@@ -1,1 +1,20 @@
-sakdlas
+@extends('layouts.navside')
+@section('da')
+<head>
+    <link rel="stylesheet" href="{{asset('frontend/css/etudiant.css')}}">
+</head>
+<div class="container">
+    <div class="btn_row">
+        <div class="cart">
+            <button>Les cartes d'etudiant</button>
+        </div>
+        <div class="cart">
+            <button>Les notes des etudiants</button>
+        </div>
+        <div class="add_btn">
+            <button>Ajouter Etudiant</button>
+        </div>
+        
+    </div>
+</div>
+@endsection
