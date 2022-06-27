9d9ab03d8fec04101714bf64d225807d54d55ff8
TonyBimenyi <tonyblaisb@gmail.com>
2022-06-27 04:06:30 -0700

    all

---
+++ resources/views/layouts/utu_top.blade.php
@@ -0,0 +1,42 @@
+<head>
+    <link rel="stylesheet" href="{{asset('frontend/css/etudiant.css')}}">
+</head>
+<div class="container">
+    <div class="header_part">
+        <div class="menu_li active">
+            <div class="icon">
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="text">
+                <p>ADMINS</p>
+            </div>
+        </div>
+        <div class="menu_li">
+            <div class="icon">
+                <i class="fa-solid fa-graduation-cap"></i>
+               
+            </div>
+            <div class="text">
+                <p>ETUDIANTS</p>
+            </div>
+        </div>
+        <div class="menu_li">
+            <div class="icon">
+                <i class="fa-solid fa-people-group"></i>
+            </div>
+            <div class="text">
+                <p>ENSEIGNANTS</p>
+            </div>
+        </div>
+        <div class="menu">
+            <div class="icon">
+                <i class="fa-solid fa-people-group"></i>
+            </div>
+            <div class="text">
+                <p>STAFF</p>
+            </div>
+        </div>
+       
+    </div>
+
+</div>
