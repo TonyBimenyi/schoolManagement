9db907778876b6dad5a9a59b31a30580f80e7cab
TonyBimenyi <tonyblaisb@gmail.com>
2022-06-29 14:10:59 -0700

    all

--- resources/views/utilisateurs/students/create_student.blade.php
+++ resources/views/utilisateurs/students/create_student.blade.php
@@ -13,33 +13,33 @@
     @include('layouts.top_row.utu_top')
     @include('layouts.second_row.student')
     <div class="form">
-        <h3 style="margin:10px 35px;color:var(--primary)">Ajouter un etudiant</h3>
+        <h3 style="margin:10px 35px;color:var(--primary)">Ajouter un étudiant</h3>
         <form action="">
             <div class="col">
             <div class="row">
                 <div class="input_row">
                     <div class="labels">
-                        <label for="">Nom et Prenom</label>
+                        <label for="">Nom</label>
                     </div>
                     <div class="input_group">
                         <div class="icon">
                             <i class="fa-solid fa-user"></i>
                         </div>
                         <div class="input">
-                            <input type="text" placeholder="Nom et Prenom">
+                            <input type="text" placeholder="Nom...">
                         </div>
                     </div>
                 </div>
                 <div class="input_row">
                     <div class="labels">
-                        <label for="">Nom d'utilisateur</label>
+                        <label for="">Prenom</label>
                     </div>
                     <div class="input_group">
                         <div class="icon">
-                            <i class="fa-solid fa-at"></i>
+                            <i class="fa-solid fa-user"></i>
                         </div>
                         <div class="input">
-                            <input type="text" placeholder="Nom d'utilisateur">
+                            <input type="text" placeholder="Prenom...">
                         </div>
                     </div>
                 </div>
@@ -58,31 +58,27 @@
                 </div>
                 <div class="input_row">
                     <div class="labels">
-                        <label for="">Sexe</label>
+                        <label for="">Date de Naissance</label>
                     </div>
                     <div class="input_group">
                         <div class="icon">
-                            <i class="fa-solid fa-mars-and-venus"></i>
+                            <i class="fa-solid fa-calendar-days"></i>
                         </div>
-                        <div class="select">
-                            <select name="" id="">
-                                <option value="" disabled>--Selection le sexe---</option>
-                                <option value="">Homme</option>
-                                <option value="">Femme</option>
-                            </select>
+                        <div class="input_date">
+                            <input class="date" type="date" placeholder="sdi">
                         </div>
                     </div>
                 </div>
                 <div class="input_row">
                     <div class="labels">
-                        <label for="">Telephone</label>
+                        <label for="">Adresse</label>
                     </div>
                     <div class="input_group">
                         <div class="icon">
-                            <i class="fa-solid fa-phone"></i>
+                            <i class="fa-solid fa-user"></i>
                         </div>
                         <div class="input">
-                            <input type="number" placeholder="+257 76 78 76">
+                            <input type="text" placeholder="Kigobe...">
                         </div>
                     </div>
                 </div>
@@ -91,44 +87,60 @@
                 <div class="row">
                     <div class="input_row">
                         <div class="labels">
-                            <label for="">Mot de passe</label>
+                            <label for="">Telephone</label>
                         </div>
                         <div class="input_group">
                             <div class="icon">
-                                <i class="fa-solid fa-lock"></i>
+                                <i class="fa-solid fa-phone"></i>
                             </div>
                             <div class="input">
-                                <input type="password" placeholder="*******************">
+                                <input type="number" placeholder="76 78 76 89">
                             </div>
                         </div>
                     </div>
                     <div class="input_row">
                         <div class="labels">
-                            <label for="">Role</label>
+                            <label for="">Promotion</label>
                         </div>
                         <div class="input_group">
                             <div class="icon">
-                                <i class="fa-brands fa-gg-circle"></i>
+                                <i class="fa-brands fa-buromobelexperte"></i>
+                            </div>
+                            <div class="input">
+                                <input type="number" placeholder="07">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input_row">
+                        <div class="labels">
+                            <label for="">Sexe</label>
+                        </div>
+                        <div class="input_group">
+                            <div class="icon">
+                                <i class="fa-solid fa-mars-and-venus"></i>
                             </div>
                             <div class="select">
                                 <select name="" id="">
-                                    <option value="" disabled>--Selectionner le role---</option>
-                                    <option value="">Super Admin</option>
-                                    <option value="">Admin</option>
+                                    <option value="" disabled>--Selection le sexe---</option>
+                                    <option value="">Homme</option>
+                                    <option value="">Femme</option>
                                 </select>
                             </div>
                         </div>
                     </div>
                     <div class="input_row">
                         <div class="labels">
-                            <label for="">Email</label>
+                            <label for="">Specialisation</label>
                         </div>
                         <div class="input_group">
                             <div class="icon">
-                                <i class="fa-solid fa-calendar-days"></i>
+                                <i class="fa-solid fa-book"></i>
                             </div>
-                            <div class="input_date">
-                                <input class="date" type="date" placeholder="sdi">
+                            <div class="select">
+                                <select name="" id="">
+                                    <option value="" disabled>--Selectionner la spec---</option>
+
+                                </select>
                             </div>
                         </div>
                     </div>
