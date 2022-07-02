47f104aa632ed2b3b72a62269f3d19df3b6a719d
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-07-02 11:34:21 +0200

    update and delete added

--- app/Http/Controllers/PersonnelController.php
+++ app/Http/Controllers/PersonnelController.php
@@ -49,30 +49,40 @@ class PersonnelController extends Controller
     }
     public function update_personnel(Request $request,$id)
     {
-        $personnel=personnel::where('id_personnel',$id)->get();
-        $personnel=new Personnel();
-        $personnel->nom_personnel=$request->input('nom');
-        $personnel->prenom_personnel=$request->input('prenom');
-        $personnel->telephone_personnel=$request->input('telephone');
-        $personnel->sexe=$request->input('sexe');
-        $personnel->email=$request->input('mail');
-        $personnel->fonction=$request->input('fonction');
-        $personnel->salaire=$request->input('salaire');
-        $personnel->update();
-        $admin=new Admin();
-        $admin->name=$request->input('nom');
-        $admin->username=$request->input('prenom');
-        $admin->email=$request->input('mail');
-        $admin->password = Hash::make($request->input('fonction'));
-        $admin->role=$request->input('fonction');
-        $admin->etat='1';
-        $admin->update();
+        // $personnel=personnel::where('id_personnel',$id)->get();
+        $personnel=DB::table('personnel')
+                        ->where('id_personnel',$id)
+                        ->update(['nom_personnel'=>$request->input('nom'),
+                                    'prenom_personnel'=>$request->input('prenom'),
+                                    'telephone_personnel'=>$request->input('telephone'),
+                                    'sexe'=>$request->input('sexe'),
+                                    'email'=>$request->input('mail'),
+                                    'fonction'=>$request->input('fonction'),
+                                    'salaire'=>$request->input('salaire')]
+                    );
+        // $personnel=new Personnel();
+        // $personnel->nom_personnel=$request->input('nom');
+        // $personnel->prenom_personnel=$request->input('prenom');
+        // $personnel->telephone_personnel=$request->input('telephone');
+        // $personnel->sexe=$request->input('sexe');
+        // $personnel->email=$request->input('mail');
+        // $personnel->fonction=$request->input('fonction');
+        // $personnel->salaire=$request->input('salaire');
+        // $personnel->update();
+        // $admin=new Admin();
+        // $admin->name=$request->input('nom');
+        // $admin->username=$request->input('prenom');
+        // $admin->email=$request->input('mail');
+        // $admin->password = Hash::make($request->input('fonction'));
+        // $admin->role=$request->input('fonction');
+        // $admin->etat='1';
+        // $admin->update();
         return redirect('personnel')->with('alert',"Personnel modified successfully");
     }
-    public function delete_personnel(Request $request)
+    public function delete_personnel($id)
     {
-        $personnel=Personnel::where('id_personnel',$id)->first();
-        $personnel->delete();
+        $personnel=Personnel::where('id_personnel',$id)->delete();
+        // $personnel->delete();
         return redirect('personnel')->with('alert',"Personnel deleted successfully");
     }
 }
