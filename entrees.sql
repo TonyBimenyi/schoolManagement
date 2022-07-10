d3aa785e950699f6f253b6345a7b9841c687029d
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-07-10 20:49:13 +0200

    table entree

---
+++ entrees.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 10, 2022 at 07:54 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `entrees`
+--
+
+CREATE TABLE `entrees` (
+  `id_entree` int(11) NOT NULL,
+  `type_entree` varchar(255) NOT NULL,
+  `designation_entree` varchar(255) NOT NULL,
+  `montant_entree` double NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `entrees`
+--
+
+INSERT INTO `entrees` (`id_entree`, `type_entree`, `designation_entree`, `montant_entree`, `created_at`, `updated_at`) VALUES
+(1, 'achat', 'achat de nouvelles equipements electroniques', 300001, NULL, NULL),
+(3, 'achat', 'achat du vehicule de DG', 23000000, '2022-07-07 07:07:52', '2022-07-07 07:07:52');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `entrees`
+--
+ALTER TABLE `entrees`
+  ADD PRIMARY KEY (`id_entree`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `entrees`
+--
+ALTER TABLE `entrees`
+  MODIFY `id_entree` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
